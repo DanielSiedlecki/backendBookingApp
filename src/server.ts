873ff6dd9ea2f58c131b0f5e8f76c90528a73af7
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-12-04 22:14:23 +0100

    define session

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,25 @@
-import express from 'express';
-import connectDB from './config/dbConnection'
-import createUser from './controllers/auth.controller'
-require('dotenv').config()
+import express from "express";
+import connectDB from "./config/dbConnection";
+import { createUser, loginUser } from "./controllers/auth.controller";
+import initPassport from './controllers/authProviders/localAuth.controller'
+require("dotenv").config();
+import session from 'express-session';
 
 connectDB();
 const app = express();
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'your-secret-key',
+  resave: false,
+  saveUninitialized: false,
+
+}));
 const port = process.env.PORT || 3030;
 app.use(express.json());
 
+initPassport(app);
+app.post("/login", loginUser);
+app.post("/register", createUser);
 
-app.post('/login', createUser);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
