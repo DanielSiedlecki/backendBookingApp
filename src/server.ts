7e51e8e886ebeb5c0b5e289fe2b524afb68734e9
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-12-06 15:53:00 +0100

    implements managment route

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,27 @@
 import express from "express";
 import connectDB from "./config/dbConnection";
 import { createUser, loginUser } from "./controllers/auth.controller";
-import routerAuth from "./routes/auth.routes"
+import routerAuth from "./routes/auth.routes";
+import routerManagment from "./routes/managment.routes";
 require("dotenv").config();
-import session from 'express-session';
+import session from "express-session";
 import initPassport from "./controllers/authProviders/localAuth.controller";
 
 connectDB();
 const app = express();
-app.use(session({
-  secret: process.env.SESSION_SECRET || 'your-secret-key',
-  resave: false,
-  saveUninitialized: false,
-
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "your-secret-key",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 const port = process.env.PORT || 3030;
 app.use(express.json());
 
 initPassport(app);
-app.use("/auth", routerAuth)
-
+app.use("/managment", routerManagment);
+app.use("/auth", routerAuth);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
