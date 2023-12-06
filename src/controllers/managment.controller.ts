1cdb23a09ff7925da10f81a31cd584176677c5a1
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-12-06 16:14:44 +0100

    created changeOpenHouer controller

--- src/controllers/managment.controller.ts
+++ src/controllers/managment.controller.ts
@@ -34,4 +34,28 @@ async function createWeek(req: Request, res: Response, next: NextFunction) {
     }
 }
 
-export { createWeek };
+async function changeOpenHouer(req: Request, res: Response) {
+    try {
+        const { dayName, opTime, clTime } = req.body
+        const result = await openingHours.findOneAndUpdate(
+            { dayOfWeek: dayName },
+            { $set: { openTime: opTime, closeTime: clTime } },
+            { new: true }
+        );
+
+
+        if (result) {
+            console.log(result);
+            return res.status(200).json({ message: "Update hours", updatedData: result });
+        } else {
+
+            return res.status(404).json({ message: "No document found for the specified dayName" });
+        }
+    }
+    catch (err) {
+        console.log(err)
+        return res.status(502).json({ message: "Error" });
+    }
+}
+
+export { createWeek, changeOpenHouer };
