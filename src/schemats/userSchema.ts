63bf4017960295bf28b4e4619c20130c9a10e3f1
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-12-08 00:42:40 +0100

    added position and services to userSchema

--- src/schemats/userSchema.ts
+++ src/schemats/userSchema.ts
@@ -5,7 +5,9 @@ interface UserDocument extends Document {
     fullname: string;
     email: string;
     password: string;
-    role: string
+    role: string;
+    position: string;
+    services: Array<string>
 }
 
 const userSchema = new Schema(
@@ -13,6 +15,8 @@ const userSchema = new Schema(
         fullname: { type: String, required: true },
         email: { type: String, required: true, unique: true },
         role: { type: String, required: true, default: "User" },
+        position: { type: String, required: false, default: "User" },
+        services: { type: Array<String>, required: false }
     },
     { timestamps: { createdAt: true } }
 );
