e43103f7e3e233b16458d1fbec08e2e18d2fee35
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-12-07 23:58:30 +0100

    add removeSpecialDay route

--- src/routes/managment.routes.ts
+++ src/routes/managment.routes.ts
@@ -10,10 +10,9 @@ router.get("/getOpenHours", managmentController.getOpenHours);
 router.get("/getAllOpenHours", managmentController.getAllOpenHours);
 
 router.get("/getSpecialDay", managmentController.getSpecialDay);
-router.get("/getAllSpecialDays", managmentController.getAllSpecialDays);
-router.post("/createSpecialDays", managmentController.createSpecialDay);
-router.put("/updateSpecialDays", managmentController.updateSpecialDay);
-
-
+router.get("/getAllSpecialDay", managmentController.getAllSpecialDays);
+router.post("/createSpecialDay", managmentController.createSpecialDay);
+router.put("/updateSpecialDay", managmentController.updateSpecialDay);
+router.delete("/removeSpecialDay", managmentController.removeSpecialDay);
 
 export default router;
