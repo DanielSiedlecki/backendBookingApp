4a81c3c7b11357faa013c689452b322a36d1d3c1
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-12-07 19:21:53 +0100

    add createspecialdays route

--- src/routes/managment.routes.ts
+++ src/routes/managment.routes.ts
@@ -1,13 +1,14 @@
-import * as express from 'express';
-import * as managmentController from '../controllers/managment.controller'
+import * as express from "express";
+import * as managmentController from "../controllers/managment.controller";
 
 const router = express.Router();
 
-router.get("/createWeek", managmentController.createWeek)
-router.put("/changeHouer", managmentController.changeOpenHouer)
+router.get("/createWeek", managmentController.createWeek);
+router.put("/changeHouer", managmentController.changeOpenHouer);
 
-router.get("/getOpenHours", managmentController.getOpenHours)
-router.get("/getAllOpenHours", managmentController.getAllOpenHours)
+router.get("/getOpenHours", managmentController.getOpenHours);
+router.get("/getAllOpenHours", managmentController.getAllOpenHours);
 
+router.post("/createSpecialDays", managmentController.createSpecialDay);
 
-export default router
+export default router;
