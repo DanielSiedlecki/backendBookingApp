b9fc1e8023777304a7691ec2517c6e77f6ba649b
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-12-07 21:23:14 +0100

    create validDateFormat func

--- src/services/dataService.ts
+++ src/services/dataService.ts
@@ -2,4 +2,15 @@ function getDayName(date = new Date(), locale = 'en-US') {
     return date.toLocaleDateString(locale, { weekday: 'long' });
 }
 
-export { getDayName };
+function validDateFormat(date: string) {
+    let dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+    if (dateRegex.test(date)) {
+        return true;
+    }
+    else {
+        return false
+    }
+
+}
+
+export { getDayName, validDateFormat };
