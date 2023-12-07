549aa21b977bf9dbd586742c6395af458bdfd2bc
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-12-07 19:04:18 +0100

    create get allopenhours

--- src/controllers/managment.controller.ts
+++ src/controllers/managment.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { openingHours, specialDays } from "../schemats/openingHouersSchema";
+import { getDayName } from "../services/dataService";
 
 async function createWeek(req: Request, res: Response, next: NextFunction) {
     try {
@@ -15,7 +16,6 @@ async function createWeek(req: Request, res: Response, next: NextFunction) {
 
         const existingDays = await openingHours.find();
         if (existingDays.length < 7) {
-
             await openingHours.deleteMany();
 
             for (const d of days) {
@@ -36,26 +36,70 @@ async function createWeek(req: Request, res: Response, next: NextFunction) {
 
 async function changeOpenHouer(req: Request, res: Response) {
     try {
-        const { dayName, opTime, clTime } = req.body
+        const { dayName, opTime, clTime } = req.body;
         const result = await openingHours.findOneAndUpdate(
             { dayOfWeek: dayName },
             { $set: { openTime: opTime, closeTime: clTime } },
             { new: true }
         );
 
-
         if (result) {
             console.log(result);
-            return res.status(200).json({ message: "Update hours", updatedData: result });
+            return res
+                .status(200)
+                .json({ message: "Update hours", updatedData: result });
         } else {
-
-            return res.status(404).json({ message: "No document found for the specified dayName" });
+            return res
+                .status(404)
+                .json({ message: "No document found for the specified dayName" });
         }
-    }
-    catch (err) {
-        console.log(err)
+    } catch (err) {
+        console.log(err);
         return res.status(502).json({ message: "Error" });
     }
 }
 
-export { createWeek, changeOpenHouer };
+async function getAllOpenHours(req: Request, res: Response) {
+    try {
+        let result = await openingHours.find({}, { _id: 0, dayOfWeek: 1, openTime: 1, closeTime: 1 });
+
+        if (result.length > 0) {
+            let days = result.map(entry => ({
+                dayName: entry.dayOfWeek,
+                openTime: entry.openTime,
+                closeTime: entry.closeTime
+            }));
+
+            return res.status(200).json({ days });
+        } else {
+            return res.status(404).json({ message: "No opening hours found" });
+        }
+    } catch (err) {
+        console.log(err);
+        return res.status(502).json({ message: "Error" });
+    }
+}
+
+async function getOpenHours(req: Request, res: Response) {
+    try {
+        const { chooseDate } = req.body;
+
+        const dayName = getDayName(new Date(chooseDate));
+
+        const result = await openingHours.findOne({
+            dayOfWeek: dayName,
+        });
+
+        if (result) {
+            return res
+                .status(200)
+                .json({ openTime: result.openTime, closeTime: result.closeTime });
+        }
+
+        return res.status(404).json({ message: "Wrong day" });
+    } catch (err) {
+        console.log(err);
+        return res.status(502).json({ message: "Error" });
+    }
+}
+export { createWeek, changeOpenHouer, getAllOpenHours, getOpenHours };
