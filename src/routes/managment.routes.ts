47926706e274c7eba6ce90462a5f7592f185614d
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-12-08 23:29:00 +0100

    correct mistake in getallspecialdays route

--- src/routes/managment.routes.ts
+++ src/routes/managment.routes.ts
@@ -10,7 +10,7 @@ router.get("/getOpenHours", managmentController.getOpenHours);
 router.get("/getAllOpenHours", managmentController.getAllOpenHours);
 
 router.get("/getSpecialDay", managmentController.getSpecialDay);
-router.get("/getAllSpecialDay", managmentController.getAllSpecialDays);
+router.get("/getAllSpecialDays", managmentController.getAllSpecialDays);
 router.post("/createSpecialDay", managmentController.createSpecialDay);
 router.put("/updateSpecialDay", managmentController.updateSpecialDay);
 router.delete("/removeSpecialDay", managmentController.removeSpecialDay);
