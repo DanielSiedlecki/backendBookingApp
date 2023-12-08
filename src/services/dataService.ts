fd0135f1a4530535191c498900914dd75a47344f
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-12-08 23:26:14 +0100

    deleted validDateFormat and add isUTCDate func

--- src/services/dataService.ts
+++ src/services/dataService.ts
@@ -2,15 +2,9 @@ function getDayName(date = new Date(), locale = 'en-US') {
     return date.toLocaleDateString(locale, { weekday: 'long' });
 }
 
-function validDateFormat(date: string) {
-    let dateRegex = /^\d{4}-\d{2}-\d{2}$/;
-    if (dateRegex.test(date)) {
-        return true;
-    }
-    else {
-        return false
-    }
-
+function isUTCDate(date: Date) {
+    return date.getTimezoneOffset() === 0;
 }
 
-export { getDayName, validDateFormat };
+
+export { getDayName, isUTCDate };
