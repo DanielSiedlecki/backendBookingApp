6669e90866788b7c4fe28d4182e5fd02b821f5cd
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-12-14 23:51:45 +0100

    add cors

--- src/server.ts
+++ src/server.ts
@@ -6,15 +6,21 @@ import routerManagment from "./routes/managment.routes";
 require("dotenv").config();
 import session from "express-session";
 import initPassport from "./controllers/authProviders/localAuth.controller";
+import cors from 'cors';
 
 connectDB();
 const app = express();
+
+const corsOptions = {
+  origin: 'http://localhost:5173'
+};
+app.use(cors(corsOptions));
 app.use(
   session({
     secret: process.env.SESSION_SECRET || "your-secret-key",
     resave: false,
     saveUninitialized: false,
-  })
+  }),
 );
 const port = process.env.PORT || 3030;
 app.use(express.json());
