10636704f48fffbbed925bef5ede1b35d8acf44b
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-12-16 23:22:26 +0100

    created delete and update Service controller

--- src/controllers/services.controller.ts
+++ src/controllers/services.controller.ts
@@ -1,43 +1,67 @@
-import Service from '../schemats/serviceSchema';
-import { Response, Request } from 'express'
+import Service from "../schemats/serviceSchema";
+import { Response, Request } from "express";
 
-async function getAllServices(res: Response, req: Request) {
-
+async function getAllServices(req: Request, res: Response) {
     try {
-
-        const services = await Service.find()
+        const services = await Service.find();
 
         if (services) {
             return res.status(200).json({ services });
+        } else {
+            return res.status(404).json({ message: "Services not found" });
         }
-        else {
-            return res.status(404).json({ message: "Services not found" })
-        }
-
     } catch (err) {
-        return res.status(502).json({ message: "error" })
+        return res.status(502).json({ message: "error" });
     }
-
-
 }
 
-async function createServices(res: Response, req: Request) {
-
+async function createServices(req: Request, res: Response) {
     try {
-
-        const { name, duration, cost } = req.body
+        const { name, duration, cost } = req.body;
 
         const service = await Service.create({ name, duration, cost });
 
-        return res.status(201).json({ message: "Service successful created" })
-
-
-
+        return res.status(201).json({ message: "Service successful created" });
     } catch (err) {
         return res.status(500).json({ message: "Error creating service" });
     }
-
-
 }
 
-export { getAllServices, createServices }
+async function updateService(req: Request, res: Response) {
+    try {
+        const { id } = req.params;
+        const { name, duration, cost } = req.body;
+
+        const updatedService = await Service.findByIdAndUpdate(
+            id,
+            { name, duration, cost },
+            { new: true }
+        );
+
+        if (!updatedService) {
+            return res.status(404).json({ message: "Service not found" });
+        }
+
+        return res
+            .status(200)
+            .json({ message: "Service successfully updated", updatedService });
+    } catch (err) {
+        return res.status(500).json({ message: "Error updating service" });
+    }
+}
+async function deleteService(req: Request, res: Response) {
+    try {
+        const { id } = req.params;
+
+        const service = await Service.findByIdAndDelete(id);
+
+        if (!service) {
+            return res.status(404).json({ message: "Service not found" });
+        }
+
+        return res.status(200).json({ message: "Service successfully deleted" });
+    } catch (err) {
+        return res.status(500).json({ message: "Error deleting service" });
+    }
+}
+export { getAllServices, createServices, updateService, deleteService };
