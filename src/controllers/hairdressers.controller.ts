27e77d689832c9f9917acc799275ac23c7905f69
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-12-17 01:21:00 +0100

    created setServiceToUser, getHairdressers

--- src/controllers/hairdressers.controller.ts
+++ src/controllers/hairdressers.controller.ts
@@ -18,3 +18,37 @@ async function setHairdresserRoleToUser(req: Request, res: Response) {
         return res.status(500).json({ message: "Error updating user role" });
     }
 }
+async function setServiceToUser(req: Request, res: Response) {
+
+    try {
+        const userId = req.params.id;
+        const serviceIds: mongoose.Types.ObjectId[] = req.body.services;
+
+        for (const serviceId of serviceIds) {
+            const serviceExists = await Service.exists({ _id: serviceId });
+            if (!serviceExists) {
+                return res.status(404).json({ message: `Service with ID ${serviceId} not found` });
+            }
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(userId, { $addToSet: { services: { $each: serviceIds } } }, { new: true }).populate('services');
+
+        if (!updatedUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        return res.status(200).json({ message: "User updated successfully", user: updatedUser });
+    } catch (err) {
+        return res.status(500).json({ message: "Error updating user" });
+    }
+}
+
+async function getHairdressers(req: Request, res: Response) {
+    try {
+
+        const hairdressers = await User.find({ role: 'Hairdresser' }).populate('services');
+        return res.status(200).json(hairdressers);
+    } catch (err) {
+        return res.status(500).json({ message: "Error fetching users" });
+    }
+}
