26d826f16d6c338d0e0d709c4c123aa0372de944
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-12-24 19:03:05 +0100

    implement sendEmail func

--- src/controllers/event.controller.ts
+++ src/controllers/event.controller.ts
@@ -1,10 +1,13 @@
-import { specialDays, SpecialDaysSchemaDocument } from "../schemats/specialDaysSchema";
+import {
+    specialDays,
+    SpecialDaysSchemaDocument,
+} from "../schemats/specialDaysSchema";
 import { openingHours } from "../schemats/openingHoursSchema";
 import { Request, Response } from "express";
-import { event, eventDocument } from '../schemats/eventSchema'
+import { event, eventDocument } from "../schemats/eventSchema";
 import { getDayName } from "../services/dataService";
+import sendEmail from "../mailer/email";
 import moment from "moment";
-
 
 async function createEvent(req: Request, res: Response) {
     try {
@@ -15,14 +18,14 @@ async function createEvent(req: Request, res: Response) {
             fullNameReserved,
             emailReserved,
             cost,
-            duration
+            duration,
         } = req.body;
-        console.log(req.body)
+        console.log(req.body);
 
         const eventStartDateTime = new Date(eventStart);
         const eventEndDateTime = new Date(eventStartDateTime);
         eventEndDateTime.setMinutes(eventStartDateTime.getMinutes() + duration);
-        console.log(eventEndDateTime)
+        console.log(eventEndDateTime);
         const newEvent: eventDocument = new event({
             employee_id,
             eventStart: eventStartDateTime,
@@ -35,61 +38,79 @@ async function createEvent(req: Request, res: Response) {
         });
 
         await newEvent.save();
-
-        res.status(201).json({ message: 'Evente succesfull created', event: newEvent });
+        await sendEmail(
+            emailReserved,
+            "Potwierdzenie wizyty",
+            "eventConfirmation",
+            { cancelLink: "www.google.com", postponeLink: "www.youtube.com" }
+        );
+        res
+            .status(201)
+            .json({ message: "Evente succesfull created", event: newEvent });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Error from server' });
+        res.status(500).json({ message: "Error from server" });
     }
 }
 
 async function getAvailableHours(req: Request, res: Response) {
     try {
-
-        const dateParam = req.query.date
+        const dateParam = req.query.date;
+        const employeeID = req.query.employeeID;
         const date = moment.utc(dateParam?.toString()).toDate();
         const dayOfWeek = getDayName(date);
-        const specialDay: SpecialDaysSchemaDocument | null = await specialDays.findOne({ date: date });
+        const specialDay: SpecialDaysSchemaDocument | null =
+            await specialDays.findOne({ date: date });
 
         let openTime: string;
         let closeTime: string;
-
+        if (typeof employeeID !== "string") {
+            return res.status(400).json({ message: "Invalid employee ID" });
+        }
         if (specialDay) {
             openTime = specialDay.openTime;
             closeTime = specialDay.closeTime;
         } else {
             const normalDay = await openingHours.findOne({ dayOfWeek: dayOfWeek });
-            console.log(normalDay)
+            console.log(normalDay);
             if (normalDay) {
                 openTime = normalDay.openTime;
                 closeTime = normalDay.closeTime;
             } else {
-                return res.status(404).json({ message: 'No opening hours information available for this day.' });
+                return res.status(404).json({
+                    message: "No opening hours information available for this day.",
+                });
             }
         }
 
         const availableHours: string[] = [];
         let currentTime = new Date(date);
-        currentTime.setHours(parseInt(openTime.split(':')[0]), parseInt(openTime.split(':')[1]));
+        currentTime.setHours(
+            parseInt(openTime.split(":")[0]),
+            parseInt(openTime.split(":")[1])
+        );
 
         const closingTime = new Date(date);
-        closingTime.setHours(parseInt(closeTime.split(':')[0]), parseInt(closeTime.split(':')[1]));
+        closingTime.setHours(
+            parseInt(closeTime.split(":")[0]),
+            parseInt(closeTime.split(":")[1])
+        );
 
         while (currentTime < closingTime) {
             const appointmentEndTime = new Date(currentTime);
             appointmentEndTime.setMinutes(appointmentEndTime.getMinutes() + 30);
 
-
             if (appointmentEndTime <= closingTime) {
-                const hours = currentTime.getHours().toString().padStart(2, '0');
-                const minutes = currentTime.getMinutes().toString().padStart(2, '0');
+                const hours = currentTime.getHours().toString().padStart(2, "0");
+                const minutes = currentTime.getMinutes().toString().padStart(2, "0");
                 const formattedTime = `${hours}:${minutes}`;
                 const eventStart = moment(currentTime).toISOString();
-                const eventEnd = moment(currentTime).add(30, 'minutes').toISOString();
+                const eventEnd = moment(currentTime).add(30, "minutes").toISOString();
 
                 const eventExists = await event.findOne({
+                    employee_id: employeeID,
                     eventStart: { $lt: eventEnd },
-                    eventEnd: { $gt: eventStart }
+                    eventEnd: { $gt: eventStart },
                 });
 
                 if (!eventExists) {
@@ -103,7 +124,7 @@ async function getAvailableHours(req: Request, res: Response) {
         res.json({ availableHours });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Server Error' });
+        res.status(500).json({ message: "Server Error" });
     }
 }
 
