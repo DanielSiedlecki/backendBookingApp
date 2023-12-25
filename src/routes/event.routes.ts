a2389689d137da9f086ca9e44cbf85d49dd65a7b
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-12-25 23:18:42 +0100

    add get,cancel, updateevent routes

--- src/routes/event.routes.ts
+++ src/routes/event.routes.ts
@@ -1,9 +1,11 @@
-import express from 'express';
-import * as eventController from '../controllers/event.controller'
+import express from "express";
+import * as eventController from "../controllers/event.controller";
 const router = express.Router();
 
-
-router.post('/createEvent', eventController.createEvent);
-router.get('/getAvailableHours', eventController.getAvailableHours)
+router.post("/createEvent", eventController.createEvent);
+router.get("/getAvailableHours", eventController.getAvailableHours);
+router.get("/getEvent/:id", eventController.getEvent);
+router.delete("/cancelEvent/:id", eventController.cancelEvent)
+router.put("/updateEvent/:id", eventController.updateEventDate)
 
 export default router;
