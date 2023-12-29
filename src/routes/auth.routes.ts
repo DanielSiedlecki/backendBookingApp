6ebb5fc4f4e1ed258dbc5a0ffa46c70371531996
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-12-29 18:12:26 +0100

    define requestPassword router

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post("/register", authController.createUser)
 
 router.post("/login", authController.loginUser)
+router.post("/requestPassword", authController.forgotPasswordRequest)
 
 export default router
