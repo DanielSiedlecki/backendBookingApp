d3a68a0ae532567d2f1eb40822eca5e4c9defa19
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-12-29 18:12:18 +0100

    created forgotpaswordrequest controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,9 @@
 import User from ".././schemats/userSchema";
 import { Request, Response, NextFunction } from "express";
+import { Token, tokenSchema } from "../schemats/tokenSchema";
 import passport from "passport";
+import crypto from "crypto"
+import sendEmail from "../mailer/email";
 
 async function createUser(req: Request, res: Response) {
   try {
@@ -11,6 +14,13 @@ async function createUser(req: Request, res: Response) {
 
     if (existingUsers.length === 0) {
       role = "Admin";
+    }
+
+    const existingUserWithEmail = await User.findOne({
+      email
+    })
+    if (existingUserWithEmail) {
+      return res.status(409).json({ message: "User with the given email is already registered" });
     }
 
     const user = new User({ name, surname, email, role });
@@ -47,4 +57,29 @@ async function loginUser(req: Request, res: Response, next: NextFunction) {
   )(req, res, next);
 }
 
-export { createUser, loginUser };
+async function forgotPasswordRequest(req: Request, res: Response) {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (user) {
+      const token = new Token({
+        userId: user._id,
+        token: crypto.randomBytes(32).toString('hex'),
+      });
+
+      const message = `${process.env.BASE_URL}/user/changepassword/${user.id}/${token.token}`;
+
+      await sendEmail(user.email, 'Change Password', 'forgotPassword', { link: message });
+
+      await token.save();
+
+      res.status(200).json({ message: 'Email sent' });
+    } else {
+      res.status(400).json({ message: 'Email is not registered' });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+}
+export { createUser, loginUser, forgotPasswordRequest };
