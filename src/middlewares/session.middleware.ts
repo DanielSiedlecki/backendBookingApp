32e3bf7781a4aacdd94660f94e9f6b722e9cb0bb
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2024-01-04 01:03:12 +0100

    define session store

--- src/middlewares/session.middleware.ts
+++ src/middlewares/session.middleware.ts
@@ -1,15 +1,19 @@
 import { NextFunction, Response, Request } from "express";
 import session from "express-session";
-const sessionMiddleware = (req: Request, res: Response, next: NextFunction) => {
+const MongoStore = require('connect-mongo')
 
+const sessionMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const dbUrl = process.env.databaseHOST
 
     return session({
         secret: process.env.SESSION_SECRET || "your-secret-key",
         resave: false,
         saveUninitialized: false,
-        cookie: { secure: false }
+        store: MongoStore.create({
+            mongoUrl: dbUrl,
+            collectionName: "session"
+        })
     })(req, res, next);
 };
-
 
 export default sessionMiddleware;
