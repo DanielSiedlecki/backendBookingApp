05c67af67421b7a25cf1c432e2d5cdc1729b6cec
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2024-01-04 01:13:58 +0100

    define logout router

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,12 +1,13 @@
-import * as express from 'express';
-import * as authController from '../controllers/auth.controller'
+import * as express from "express";
+import * as authController from "../controllers/auth.controller";
 
 const router = express.Router();
 
-router.post("/register", authController.createUser)
+router.post("/register", authController.createUser);
 
-router.post("/login", authController.loginUser)
-router.post("/requestPassword", authController.forgotPasswordRequest)
-router.put("/changePassword", authController.changePasswordWithToken)
+router.post("/login", authController.loginUser);
+router.post("/requestPassword", authController.forgotPasswordRequest);
+router.put("/changePassword", authController.changePasswordWithToken);
+router.post("/logout", authController.logoutSession);
 
-export default router
+export default router;
