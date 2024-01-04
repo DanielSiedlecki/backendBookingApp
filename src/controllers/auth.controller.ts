3d344b27feb0f2afbe9758aa44f65a2ff94f0c09
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2024-01-04 01:02:46 +0100

    created changePasswordWithToken and refactor login

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,9 +1,10 @@
-import User from ".././schemats/userSchema";
+import User, { UserDocument } from ".././schemats/userSchema";
 import { Request, Response, NextFunction } from "express";
 import { Token, tokenSchema } from "../schemats/tokenSchema";
 import passport from "passport";
 import crypto from "crypto"
 import sendEmail from "../mailer/email";
+
 
 async function createUser(req: Request, res: Response) {
   try {
@@ -34,27 +35,26 @@ async function createUser(req: Request, res: Response) {
 }
 
 async function loginUser(req: Request, res: Response, next: NextFunction) {
-  passport.authenticate(
-    "local",
-    { session: true },
-    (err: Error, user: Object, info: string) => {
-      if (err) {
-        return next(err);
-      }
-
-      if (!user) {
-        return res.status(401).json({ message: "Authentication failed", info });
-      }
-
-      req.login(user, (loginErr) => {
-        if (loginErr) {
-          return next(loginErr);
-        }
-
-        return res.status(200).json({ message: "Login successful", user });
-      });
+  passport.authenticate('local', { session: true }, (err: Error, user: UserDocument, info: any) => {
+    if (err) {
+      return next(err);
     }
-  )(req, res, next);
+
+    if (!user) {
+      return res.status(401).json({ error: 'Authentication failed', message: info.message });
+    }
+
+    req.logIn(user, (loginErr) => {
+      if (loginErr) {
+        return next(loginErr);
+      }
+      if (req.isAuthenticated()) {
+        return res.json({ message: 'Login successful', user, authorized: true });
+      } else {
+        return res.status(403).json({ error: 'Unauthorized', message: 'User does not have the required permissions.' });
+      }
+    });
+  })(req, res, next);
 }
 
 async function forgotPasswordRequest(req: Request, res: Response) {
@@ -82,4 +82,39 @@ async function forgotPasswordRequest(req: Request, res: Response) {
     res.status(500).json({ message: 'Internal Server Error' });
   }
 }
-export { createUser, loginUser, forgotPasswordRequest };
+async function changePasswordWithToken(req: Request, res: Response) {
+  const { userId, token, newPassword } = req.body;
+
+  try {
+
+    const resetToken = await Token.findOne({ userId, token });
+
+    if (!resetToken) {
+      return res.status(400).json({ error: 'Invalid token' });
+    }
+
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+
+    user.setPassword(newPassword, async () => {
+      await user.save();
+
+
+      await resetToken.remove();
+
+      return res.status(200).json({ message: 'Password changed successfully' });
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
+
+
+
+export { createUser, loginUser, forgotPasswordRequest, changePasswordWithToken, checkAuthorization };
