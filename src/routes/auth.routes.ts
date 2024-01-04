26e0dd9a13c39ac8fb7b026f111680a0955c53df
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2024-01-04 03:13:13 +0100

    defined requestpasswordverify route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -4,9 +4,9 @@ import * as authController from "../controllers/auth.controller";
 const router = express.Router();
 
 router.post("/register", authController.createUser);
-
 router.post("/login", authController.loginUser);
 router.post("/requestPassword", authController.forgotPasswordRequest);
+router.post("/requestPasswordVerify", authController.requestPasswordVerify)
 router.put("/changePassword", authController.changePasswordWithToken);
 router.post("/logout", authController.logoutSession);
 
