6f666d6870e935c896cb686905039213e0d7c979
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2024-01-09 01:04:34 +0100

    define getallevents and ended event routes

--- src/routes/event.routes.ts
+++ src/routes/event.routes.ts
@@ -5,7 +5,9 @@ const router = express.Router();
 router.post("/createEvent", eventController.createEvent);
 router.get("/getAvailableHours", eventController.getAvailableHours);
 router.get("/getEvent/:id", eventController.getEvent);
-router.delete("/cancelEvent/:id", eventController.cancelEvent)
-router.put("/updateEvent/:id", eventController.updateEventDate)
+router.get("/getAllEvents/:userid", eventController.getAllEvents);
+router.delete("/cancelEvent/:id", eventController.cancelEvent);
+router.put("/updateEvent/:id", eventController.updateEventDate);
+router.put("/endedEvent/:id", eventController.endedEvent)
 
 export default router;
